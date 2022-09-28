a5edd03db38d7702c32f82fb404ca85d552630ab
jpleorx <leo.ertuna@gmail.com>
2022-09-28 13:53:16 +0300

    Save OdSample as labelme dataset

--- tekleo_common_utils_ai/__init__.py
+++ tekleo_common_utils_ai/__init__.py
@@ -1,10 +1,12 @@
 from .utils_dataset_google import UtilsDatasetGoogle
+from .utils_dataset_labelme import UtilsDatasetLabelme
 from .utils_dataset_pascalvoc import UtilsDatasetPascalvoc
 from .utils_detectron_model import UtilsDetectronModel
 from .utils_visualize_od import UtilsVisualizeOd
 
 __all__ = [
     UtilsDatasetGoogle,
+    UtilsDatasetLabelme,
     UtilsDatasetPascalvoc,
     UtilsDetectronModel,
     UtilsVisualizeOd
