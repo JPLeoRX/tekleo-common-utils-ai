d3fa26a635c148482ea0306caa63a216c6a89f73
jpleorx <leo.ertuna@gmail.com>
2022-10-26 23:16:07 +0300

    Rework google dataset utils to work with new masks

--- tekleo_common_utils_ai/utils_dataset_google.py
+++ tekleo_common_utils_ai/utils_dataset_google.py
@@ -1,12 +1,11 @@
 import json
-import os
 import tempfile
 from typing import List
 import concurrent.futures
 from itertools import repeat
 from google.cloud import storage
 from injectable import injectable, autowired, Autowired
-from tekleo_common_message_protocol import OdSample, OdLabeledBox, RectangleRelative
+from tekleo_common_message_protocol import OdSample, OdLabeledItem, PointRelative
 from tekleo_common_utils import UtilsImage
 
 
@@ -26,32 +25,29 @@ class UtilsDatasetGoogle:
         od_sample_name = image_gcs_uri.split('/')[-1].split('.')[0]
         image_file_name = image_gcs_uri.split('/')[-1]
         image_file_path = tempfile.gettempdir() + '/' + image_file_name
-        # print('UtilsDatasetGoogle.load_sample_from_jsonl_line(): od_sample_name=' + str(od_sample_name) + ', image_file_name=' + str(image_file_name) + ', image_file_path=' + str(image_file_path))
 
         # Download the image, create client, enter a bucket, select blob and download it
         storage_client = storage.Client.from_service_account_json(json_credentials_filepath)
-        bucket = storage_client.bucket("ml_image_stage")
-        blob_name = image_gcs_uri.replace('gs://ml_image_stage/', '')
-        blob = bucket.blob(blob_name)
-        blob.download_to_filename(image_file_path)
+        with open(image_file_path, "wb") as file_obj:
+            storage_client.download_blob_to_file(image_gcs_uri, file_obj)
 
         # Open the image
         image_pil = self.utils_image.open_image_pil(image_file_path)
 
         # Parse boxes
-        od_sample_boxes = []
+        od_sample_items = []
         for bounding_box_annotation in bounding_box_annotations:
             x = bounding_box_annotation['xMin']
             y = bounding_box_annotation['yMin']
             w = bounding_box_annotation['xMax'] - x
             h = bounding_box_annotation['yMax'] - y
-            od_labeled_box_region = RectangleRelative(x, y, w, h)
-            od_labeled_box_label = bounding_box_annotation['displayName']
-            od_labeled_box = OdLabeledBox(od_labeled_box_label, od_labeled_box_region)
-            od_sample_boxes.append(od_labeled_box)
+            od_labeled_item_mask = [PointRelative(x, y), PointRelative(x + w, y), PointRelative(x + w, y + h), PointRelative(x, y + h)]
+            od_labeled_item_label = bounding_box_annotation['displayName']
+            od_labeled_item = OdLabeledItem(od_labeled_item_label, od_labeled_item_mask)
+            od_sample_items.append(od_labeled_item)
 
         # Create finalize sample
-        return OdSample(od_sample_name, image_pil, od_sample_boxes)
+        return OdSample(od_sample_name, image_pil, od_sample_items)
 
     def load_samples_from_jsonl(self, json_credentials_filepath: str, jsonl_data_file_path: str) -> List[OdSample]:
         # Read JSON file
