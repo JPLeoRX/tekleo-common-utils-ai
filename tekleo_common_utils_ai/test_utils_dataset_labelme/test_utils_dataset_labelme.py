47346a52836bed95e804efd1f721bcbd3c1ed6d5
jpleorx <leo.ertuna@gmail.com>
2022-10-27 13:11:35 +0300

    Fixed paths in test labelme

--- tekleo_common_utils_ai/test_utils_dataset_labelme/test_utils_dataset_labelme.py
+++ tekleo_common_utils_ai/test_utils_dataset_labelme/test_utils_dataset_labelme.py
@@ -11,7 +11,7 @@ utils_dataset_labelme = UtilsDatasetLabelme()
 utils_dataset_coco = UtilsDatasetCoco()
 
 # Open labelme
-labelme_folder_path = "/tekleo_common_utils_ai/test_utils_dataset_labelme/dataset_labelme"
+labelme_folder_path = "dataset_labelme"
 s1 = utils_dataset_labelme.load_sample_from_image_and_labelme_json(labelme_folder_path + "/signs1.jpg", labelme_folder_path + "/signs1.json")
 print(s1)
 s2 = utils_dataset_labelme.load_sample_from_folder("signs1", labelme_folder_path)
@@ -20,5 +20,5 @@ s3 = utils_dataset_labelme.load_samples_from_folder(labelme_folder_path)[0]
 print(s3)
 
 # Save to coco
-coco_folder_path = "/tekleo_common_utils_ai/test_utils_dataset_labelme/dataset_coco"
+coco_folder_path = "dataset_coco"
 utils_dataset_coco.save_samples_to_folder([s3], coco_folder_path)
