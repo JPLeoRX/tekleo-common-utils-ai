1365ce96f06cf35895a1fa358402b51530350e8d
leo <leo.ertuna@gmail.com>
2022-11-16 02:10:32 +0300

    Bump up version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme_file:
 
 setup_args = dict(
     name='tekleo-common-utils-ai',
-    version='0.0.0.3',
+    version='0.0.0.4',
     description="",
     keywords=[],
     long_description=readme_text,
