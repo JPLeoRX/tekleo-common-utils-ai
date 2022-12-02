b8cf3b2143177c1a2e1052847f81093e598a1845
jpleorx <leo.ertuna@gmail.com>
2022-12-02 16:38:04 +0300

    Testing modifiers

--- tekleo_common_utils_ai/test_utils_dataset_pipes/test_utils_dataset_pipes.py
+++ tekleo_common_utils_ai/test_utils_dataset_pipes/test_utils_dataset_pipes.py
@@ -8,7 +8,9 @@ from tekleo_common_utils_ai.dataset_modification.dataset_modifier_brightness imp
 from tekleo_common_utils_ai.dataset_modification.dataset_modifier_contrast import DatasetModifierContrast
 from tekleo_common_utils_ai.dataset_modification.dataset_modifier_crop import DatasetModifierCrop
 from tekleo_common_utils_ai.dataset_modification.dataset_modifier_blur import DatasetModifierBlur
-
+from tekleo_common_utils_ai.dataset_modification.dataset_modifier_saturation import DatasetModifierSaturation
+from tekleo_common_utils_ai.dataset_modification.dataset_modifier_hue import DatasetModifierHue
+from tekleo_common_utils_ai.dataset_modification.dataset_modifier_sharpen import DatasetModifierSharpen
 load_injection_container(str(os.path.dirname(tekleo_common_utils.__file__)))
 load_injection_container('../')
 from tekleo_common_utils import UtilsImage
@@ -23,24 +25,38 @@ utils_image = UtilsImage()
 labelme_folder_path = "/Users/leo/tekleo/tekleo-common-utils-ai/tekleo_common_utils_ai/test_utils_dataset_pipes/dataset_labelme_original"
 coco_folder_path = "/Users/leo/tekleo/tekleo-common-utils-ai/tekleo_common_utils_ai/test_utils_dataset_pipes/dataset_coco_modified"
 samples = utils_dataset_labelme.load_samples_from_folder(labelme_folder_path)
-print(samples[0])
 
-#
-mod_brightness = DatasetModifierBrightness(0.08, 0.28, "increase", 7)
-mod_contrast = DatasetModifierContrast(0.05, 0.20, "decrease", 7)
+# Declare modifiers
+mod_brightness_increase = DatasetModifierBrightness(0.08, 0.28, "increase", 7)
+mod_brightness_decrease = DatasetModifierBrightness(0.08, 0.28, "decrease", 7)
+mod_contrast_increase = DatasetModifierContrast(0.05, 0.20, "increase", 7)
+mod_contrast_decrease = DatasetModifierContrast(0.05, 0.20, "decrease", 7)
+mod_saturation_increase = DatasetModifierSaturation(0.1, 0.8, "increase", 7)
+mod_saturation_decrease = DatasetModifierSaturation(0.05, 0.5, "decrease", 7)
+mod_hue_increase = DatasetModifierHue(0.01, 0.06, "increase", 7)
+mod_hue_decrease = DatasetModifierHue(0.01, 0.06, "decrease", 7)
+mod_blur = DatasetModifierBlur(0.01, 0.03, 7)
+mod_sharpen = DatasetModifierSharpen(0.01, 0.05, 7)
 mod_crop_x = DatasetModifierCrop(0.05, 0.75, "x", 3)
 mod_crop_y = DatasetModifierCrop(0.05, 0.75, "y", 5)
 mod_crop_both = DatasetModifierCrop(0.05, 0.75, "both", 7)
-mod_blur = DatasetModifierBlur(min_blur_ratio=0.01, max_blur_ratio=0.03, random_seed=7)
-pipe = DatasetModificationPipe([
-    # mod_contrast,
-    # DatasetModifierBorder(0.02, 0.08, "x", [(0, 0, 0)], 9),
-    # DatasetModifierBorder(0.02, 0.08, "y", [(0, 0, 0)], 8),
-    # DatasetModifierCrop(0.1, 0.8, "y", 9)
-    mod_crop_x, mod_crop_y, mod_crop_both, mod_blur
-], behavior_dataset_ratio_to_process=1.0, behavior_random=BehaviorRandom.ALL, behavior_random_seed=7, behavior_chaining=BehaviorChaining.UNIQUE, behavior_originals=BehaviorOriginals.OVERWRITE)
-new_samples = pipe.process(samples)
-# for s in new_samples:
-#     utils_image.debug_image_pil(s.image)
+mod_border_x = DatasetModifierBorder(0.02, 0.08, "x", [(0, 0, 0)], 9)
+mod_border_y = DatasetModifierBorder(0.02, 0.08, "y", [(0, 0, 0)], 8)
+mod_border_both = DatasetModifierBorder(0.02, 0.08, "both", [(0, 0, 0)], 8)
 
+# Declare pipe
+pipe = DatasetModificationPipe([
+    mod_brightness_increase, mod_brightness_decrease,
+    mod_contrast_increase, mod_contrast_decrease,
+    mod_saturation_increase, mod_saturation_decrease,
+    mod_hue_increase, mod_hue_decrease,
+    mod_blur, mod_sharpen,
+    mod_crop_x, mod_crop_y, mod_crop_both,
+    mod_border_x, mod_border_y, mod_border_both,
+], behavior_dataset_ratio_to_process=1.0, behavior_random=BehaviorRandom.ALL, behavior_random_seed=7, behavior_chaining=BehaviorChaining.UNIQUE, behavior_originals=BehaviorOriginals.OVERWRITE)
+
+# Apply pipe
+new_samples = pipe.process(samples)
+
+# Export dataset to COCO
 utils_dataset_coco.save_samples_to_folder(new_samples, coco_folder_path)
